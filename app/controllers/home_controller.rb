9cc83d6051750adce42de916e2bbcda237e37f7c
Camille-v2 <m226828@WK2TK82102CRMID.academy.usna.edu>
2022-02-09 13:38:28 -0500

    wednesday commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,19 @@
+class HomeController < ApplicationController
+    def index
+    end
+
+    def featured
+        @products = products.
+    end
+
+    def cat1
+        @cat1.
+    end
+
+    def cat2
+    end
+
+    def secret
+        @secret_message = "go army"
+    end
+end
