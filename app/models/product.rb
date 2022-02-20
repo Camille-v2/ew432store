62b23ed4d5a05ad00fc48558b60252999257df82
Camille-v2 <m226828@WK2TK82102CRMID.academy.usna.edu>
2022-02-20 17:56:55 -0500

    Can Add Products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
     validates :name, presence: true
+    validates :description, presence: true, length: {minimum: 10}
     validates :price, comparison: {greater_than: 0}
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "home#index"
-  get "/secret", to: "home#secret"
+  root "products#index"
   
+  # get "/products", to:"products#index"
+  # get "/products/:id", to: "products#show"
+  
+  resources :products
   # Defines the root path route ("/")
   # root "articles#index"
 end
